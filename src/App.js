b2523952c0a163486bd5f86e48807a68acd60762
Anasemos Kassahun <anasemosaka@gmail.com>
2022-09-26 23:17:44 -0400

    added sass and categories

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './categories.styles.scss'
 function App() {
+
+  const categories = [
+
+    {
+      "id": 1,
+      "title": "All",
+      "imageUrl": "https://images.unsplash.com/photo-1483412033650-1015ddeb83d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80"
+    },
+    {
+      "id": 2,
+      "title": "Reggae",
+      "imageUrl": "https://images.unsplash.com/photo-1538598450935-581f6a5fa7e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1488&q=80"
+    },
+    {
+      "id": 3,
+      "title": "Pop",
+      "imageUrl": "https://images.unsplash.com/photo-1512830414785-9928e23475dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    },
+    {
+      "id": 4,
+      "title": "Hip-Hop",
+      "imageUrl": "https://images.unsplash.com/photo-1601643157091-ce5c665179ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"
+    },
+    {
+      "id": 5,
+      "title": "Jazz",
+      "imageUrl": "https://images.unsplash.com/photo-1415201364774-f6f0bb35f28f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    }
+  ]
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      {categories.map(({ title, id, imageUrl }) => (
+        <div key={id} className="category-container">
+          <div className="background-image" style={{
+            backgroundImage: `url(${imageUrl})`
+          }} />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+
     </div>
   );
 }
