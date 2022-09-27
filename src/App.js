1c5bdc26e30a2a5eb323250ec8fe44a082311831
Anasemos Kassahun <anasemosaka@gmail.com>
2022-09-26 23:53:11 -0400

    updated directory page with directory and category component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import './categories.styles.scss'
-function App() {
+import Directory from './components/directory/directory.component';
+
+
+const App = () => {
 
   const categories = [
 
@@ -32,20 +34,7 @@ function App() {
 
 
   return (
-    <div className="categories-container">
-      {categories.map(({ title, id, imageUrl }) => (
-        <div key={id} className="category-container">
-          <div className="background-image" style={{
-            backgroundImage: `url(${imageUrl})`
-          }} />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-
-    </div>
+    <Directory categories={categories} />
   );
 }
 
